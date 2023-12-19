60127767efd025e5c73cc44399ff669ba3e52ee6
gialerss <77748135+gialerss@users.noreply.github.com>
2023-12-19 21:47:33 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,1 @@
-
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; 
-document.getElementsByTagName('head')[0].appendChild(script);
-
-
-$(document).ready(function(){
-    $("button").remove();
-})
+alert("ciao davide")
