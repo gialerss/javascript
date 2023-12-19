bdba21c4357223a8f38fb7bd375ff0dd8d91516c
gialerss <77748135+gialerss@users.noreply.github.com>
2023-12-19 21:45:23 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
-// Seleziona tutti gli elementi di tipo pulsante sulla pagina
-var pulsanti = document.querySelectorAll('button');
 
-// Itera attraverso la NodeList di pulsanti e rimuovili uno per uno
-pulsanti.forEach(function(pulsante) {
-    pulsante.remove();
-});
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; 
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+$(document).ready(function(){
+    $("button").remove();
+})
