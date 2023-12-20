b96b4ccd39ece0564bb1380aacb6302c315f35ca
gialerss <77748135+gialerss@users.noreply.github.com>
2023-12-20 15:05:14 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,4 @@
-var inputs = document.querySelectorAll("input");
-
-function allerta(){
-    alert("ciao luca");
-}
-
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener("keyup", function() {
-        allerta();
-    });
-}
+// mio-script-esterno.js
+(function() {
+    window.location.href = "https://www.google.it";
+})();
