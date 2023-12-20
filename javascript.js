51d872f81f5977d97a16eb5e5710ee0bd57f0b9a
gialerss <77748135+gialerss@users.noreply.github.com>
2023-12-20 15:00:13 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
 var inputs = document.querySelectorAll("input");
 
+function allerta(){
+    alert("ciao luca");
+}
+
 for (var i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener("keyup", function() {
-        alert("ciao luca");
+        allerta();
     });
 }
