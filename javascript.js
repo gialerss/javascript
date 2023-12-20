b36b5575e39143a65b3a42151940674edd88bd8b
gialerss <77748135+gialerss@users.noreply.github.com>
2023-12-20 14:44:31 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-alert("ciao")
+// Seleziona tutti gli elementi con la classe 'form-control'
+var formControls = document.getElementsByClassName("form-control");
+
+// Itera su tutti gli elementi form-control e assegna la funzione clearFormOnKeyUp all'evento onkeyup
+for (var i = 0; i < formControls.length; i++) {
+ formControls[i].onkeyup = clearFormOnKeyUp;
+}
+
+function clearFormOnKeyUp() {
+ // Reset the value of the input field to an empty string
+ this.value = "";
+}
